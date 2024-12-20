934ce01f2161a43083b53ca417137769b593cb9d
Austin Adams <git@austinjadams.com>
2024-12-20 15:14:55 -0500

    Attempt Apptainer support

--- ci/build-manylinux/build-common.sh
+++ ci/build-manylinux/build-common.sh
@@ -10,6 +10,8 @@ whereami_absolute=$repo_path_absolute/$whereami_relative
 dockerfile_path=$whereami_absolute/Dockerfile
 plat=manylinux_2_28_x86_64
 image_name=$plat-qwerty
+rg_container_dir=/projects/ci-runners/qwerty-llvm-builds/
+rg_container_path=/projects/ci-runners/qwerty-llvm-builds/$image_name.sif
 
 # Useful for security-minded individuals who heed the warning at the top of
 # this page: https://wiki.debian.org/Docker. The default is for the more
@@ -20,11 +22,6 @@ else
     docker_sudo=()
 fi
 
-image_sha=$("${docker_sudo[@]}" docker images -q "$image_name")
-if [[ -z $image_sha ]]; then
-    "${docker_sudo[@]}" docker build -t "$image_name" -f "$dockerfile_path" --build-arg PLAT="$plat" "$repo_path_absolute"
-fi
-
 io_path_absolute=$whereami_absolute/io
 mkdir -p "$io_path_absolute"
 guest_repo_path_absolute=/io/
@@ -32,6 +29,15 @@ guest_whereami_path_absolute=$guest_repo_path_absolute/$whereami_relative
 guest_io_path_absolute=$guest_whereami_path_absolute/io
 guest_script_path_absolute=$guest_whereami_path_absolute/guest-scripts
 
+build_docker_if_needed() {
+    image_sha=$("${docker_sudo[@]}" docker images -q "$image_name")
+    if [[ -z $image_sha ]]; then
+        "${docker_sudo[@]}" docker build -t "$image_name" -f "$dockerfile_path" --build-arg PLAT="$plat" "$repo_path_absolute"
+    fi
+}
+
 run_docker() {
+    build_docker_if_needed
+
     "${docker_sudo[@]}" docker run --rm -v "$repo_path_absolute:$guest_repo_path_absolute" "$image_name" "$@"
 }
--- ci/build-manylinux/build-manylinux-llvm.sh
+++ ci/build-manylinux/build-manylinux-llvm.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 set -e -o pipefail
 
-if [[ $# -ne 1 ]]; then
-    printf 'usage: %s <llvm-version>\n' "$0" >&2
+if [[ $# -ne 2 || ( ( $1 != docker ) && ( $1 != apptainer ) ) ]]; then
+    printf 'usage: %s <docker|apptainer> <llvm-version>\n' "$0" >&2
     exit 1
 fi
-llvm_version=$1
+container=$1
+llvm_version=$2
 
 . "$(dirname "${BASH_SOURCE[0]}")/build-common.sh"
 
-run_docker "$guest_whereami_path_absolute/../build-llvm.sh" "$llvm_version" "$guest_io_path_absolute"
+if [[ $container == docker ]]; then
+    run_docker "$guest_whereami_path_absolute/../build-llvm.sh" "$llvm_version" "$guest_io_path_absolute"
+elif [[ $container == apptainer ]]; then
+    apptainer exec --bind "$repo_path_absolute:$guest_repo_path_absolute" "$rg_container_path" "$guest_whereami_path_absolute/../build-llvm.sh" "$llvm_version" "$guest_io_path_absolute"
+else
+    printf 'unknown type of container' >&2
+    exit 1
+fi
