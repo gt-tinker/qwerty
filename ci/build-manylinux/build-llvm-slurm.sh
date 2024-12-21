55bdbe29217309611bc382196e115a23a6995075
Austin Adams <git@austinjadams.com>
2024-12-20 21:44:12 -0500

    Request specific number of cpus

--- ci/build-manylinux/build-llvm-slurm.sh
+++ ci/build-manylinux/build-llvm-slurm.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-#SBATCH -Jqwerty-llvm-ci  # Job name
-#SBATCH -t 03:00:00       # Duration of the job (3 hours)
-#SBATCH -w hawksbill      # Partition Name
+#SBATCH -Jqwerty-llvm-ci        # Job name
+#SBATCH -N1 --cpus-per-task=32  # Number of nodes and CPUs per node required
+#SBATCH --mem-per-cpu=4G        # Memory per core
+#SBATCH -t 03:00:00             # Duration of the job (3 hours)
+#SBATCH -w hawksbill            # Partition Name
 #SBATCH -o /tools/ci-reports/qwerty-llvm-ci-%j.out   # Combined output and error messages file
-#SBATCH -W                # Do not exit until the submitted job terminates.
+#SBATCH -W                      # Do not exit until the submitted job terminates.
 
 cd "$GITHUB_WORKSPACE/qwerty"
 hostname
