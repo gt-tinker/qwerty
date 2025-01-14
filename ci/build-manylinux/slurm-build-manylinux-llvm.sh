c5f66a0c4fb5a17d27d71920f5f6a7d1baa84969
Austin Adams <git@austinjadams.com>
2025-01-14 13:25:16 -0500

    llvm build: Fix slurm command line

--- ci/build-manylinux/slurm-build-manylinux-llvm.sh
+++ ci/build-manylinux/slurm-build-manylinux-llvm.sh
@@ -3,7 +3,7 @@
 #SBATCH -N1 --cpus-per-task=32  # Number of nodes and CPUs per node required
 #SBATCH --mem-per-cpu=2G        # Memory per core
 #SBATCH -t 03:00:00             # Duration of the job (3 hours)
-#SBATCH -w rg-nextgen-hpc       # Partition Name
+#SBATCH -p rg-nextgen-hpc       # Partition Name
 #SBATCH -o /tools/ci-reports/qwerty-manylinux-llvm-ci-%j.out   # Combined output and error messages file
 #SBATCH -W                      # Do not exit until the submitted job terminates.
 
