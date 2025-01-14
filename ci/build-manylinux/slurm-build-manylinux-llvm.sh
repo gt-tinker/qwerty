e24c03f532e817bcfad5d7b6563cdbc80944e51d
Austin Adams <git@austinjadams.com>
2025-01-14 13:22:02 -0500

    Update slurm LLVM script for hawksbill's death
    May it RIP in eternal pizza

--- ci/build-manylinux/slurm-build-manylinux-llvm.sh
+++ ci/build-manylinux/slurm-build-manylinux-llvm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -Jqwerty-llvm-ci        # Job name
 #SBATCH -N1 --cpus-per-task=32  # Number of nodes and CPUs per node required
-#SBATCH --mem-per-cpu=4G        # Memory per core
+#SBATCH --mem-per-cpu=2G        # Memory per core
 #SBATCH -t 03:00:00             # Duration of the job (3 hours)
-#SBATCH -w hawksbill            # Partition Name
+#SBATCH -w rg-nextgen-hpc       # Partition Name
 #SBATCH -o /tools/ci-reports/qwerty-manylinux-llvm-ci-%j.out   # Combined output and error messages file
 #SBATCH -W                      # Do not exit until the submitted job terminates.
 
