2ea2054d53c228930c93bb5b82948e482fc30054
Austin Adams <git@austinjadams.com>
2025-08-06 00:54:47 -0400

    Remove CCirc/Transforms
    Unused and unlikely to be used in the near future. This commit can be
    reverted if we change our mind.

--- qwerty_mlir/lib/CCirc/IR/CCircOps.cpp
+++ qwerty_mlir/lib/CCirc/IR/CCircOps.cpp
@@ -9,7 +9,6 @@
 #include "mlir/Dialect/Arith/IR/Arith.h"
 #include "CCirc/IR/CCircOps.h"
 #include "CCirc/IR/CCircDialect.h"
-#include "CCirc/Transforms/CCircPasses.h"
 
 #define GET_OP_CLASSES
 #include "CCirc/IR/CCircOps.cpp.inc"
