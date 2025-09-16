5e1e536379c19c73dbcbfdb45415dc50afb03e68
Austin Adams <git@austinjadams.com>
2025-09-16 00:42:01 -0400

    Prototype for XAG circuit synth

---
+++ qwerty_mlir/lib/QCirc/Utils/SynthBennett.cpp
@@ -0,0 +1,9 @@
+#include "QCirc/Utils/QCircUtils.h"
+
+void synthBennettFromXAG(
+        mlir::OpBuilder &builder,
+        mlir::Location loc,
+        ccirc::CircuitOp xag_circ,
+        llvm::SmallVectorImpl<mlir::Value> &qubits,
+        size_t qubit_idx) {
+}
