2ac62763e3cc1f50cddbf5b1af3a1788015d1f60
Austin Adams <git@austinjadams.com>
2025-09-24 11:43:14 -0400

    Tiny typo in website example program

--- examples/abbrev/deutsch_driver.py
+++ examples/abbrev/deutsch_driver.py
@@ -9,7 +9,7 @@ def balanced(x: bit) -> bit:
 def constant(x: bit) -> bit:
     return bit[1](0b1)
 
-print('If f(x) = ~x,')
+print('If f(x) = ~x:')
 print('f(0) xor f(1) =', deutsch(balanced))
 print('If f(x) = 1:')
 print('f(0) xor f(1) =', deutsch(constant))
