8fe46bb7f1beb6d5358250b1e8cd4a3b64d37b2d
Austin Adams <git@austinjadams.com>
2026-01-09 16:29:25 -0500

    Clean up visitor code a bit

--- qwerty_ast_macros/src/visitor.rs
+++ qwerty_ast_macros/src/visitor.rs
@@ -1,13 +1,116 @@
 use crate::syn_util::paths;
 use proc_macro::TokenStream;
+use proc_macro2::{Span, TokenStream as TokenStream2};
 use quote::quote_spanned;
-use syn::{Arm, Block, Error, Expr, ExprBlock, Macro, Stmt, StmtMacro, Token, spanned::Spanned};
+use syn::{
+    Arm, Block, Error, Expr, ExprBlock, Macro, Pat, Stmt, StmtMacro, Token, spanned::Spanned,
+};
 
 mod parse;
 
 enum Chunk {
     Visit(Expr),
     Hook(Vec<Stmt>),
+}
+
+fn block_to_chunks(block: Block) -> Result<Vec<Chunk>, Error> {
+    let Block { stmts, .. } = block;
+
+    let mut chunks = Vec::new();
+    let mut pending_stmts = Vec::new();
+
+    for stmt in stmts {
+        match stmt {
+            Stmt::Macro(StmtMacro {
+                mac: Macro { path, tokens, .. },
+                ..
+            }) if paths::path_is_ident_str(&path, "visit") => {
+                if !pending_stmts.is_empty() {
+                    chunks.push(Chunk::Hook(pending_stmts));
+                    pending_stmts = Vec::new();
+                }
+
+                let arg: Expr = syn::parse2(tokens)?;
+                chunks.push(Chunk::Visit(arg));
+            }
+
+            other_stmt => {
+                pending_stmts.push(other_stmt);
+            }
+        }
+    }
+
+    if !pending_stmts.is_empty() {
+        chunks.push(Chunk::Hook(pending_stmts));
+    }
+
+    Ok(chunks)
+}
+
+fn chunks_into_ent_arm(pat: &Pat, span: Span, mut chunks: Vec<Chunk>) -> TokenStream2 {
+    if chunks.is_empty() {
+        quote_spanned! {span=>
+            StackEntry::Node(#pat) => {}
+        }
+    } else {
+        // So we can treat it as a stack
+        chunks.reverse();
+
+        let first_chunk = chunks.pop().expect("chunks is nonempty yet empty");
+        let (leading_hook_stmts, extra_first_chunk) = if let Chunk::Hook(stmts) = first_chunk {
+            (stmts, None)
+        } else {
+            (Vec::new(), Some(first_chunk))
+        };
+
+        let mut hook_counter = 0usize;
+        let (push_stmts, mut hook_arms): (Vec<_>, Vec<_>) = extra_first_chunk
+            .into_iter()
+            .chain(chunks.into_iter())
+            .map(|chunk| {
+                match chunk {
+                    Chunk::Visit(expr) => {
+                        (
+                            quote_spanned! {span=>
+                                stack.push(StackEntry::Node(&#expr));
+                            },
+                            quote_spanned! {span=>
+                                // No hook arm needed
+                            },
+                        )
+                    }
+
+                    Chunk::Hook(stmts) => {
+                        let hook_id = hook_counter;
+                        hook_counter += 1;
+
+                        (
+                            quote_spanned! {span=>
+                                stack.push(StackEntry::Hook(#hook_id, node));
+                            },
+                            quote_spanned! {span=>
+                                #[allow(unused)]
+                                StackEntry::Hook(#hook_id, #pat) => {
+                                    #(#stmts)*
+                                }
+                            },
+                        )
+                    }
+                }
+            })
+            .unzip();
+        // For readability of generated code
+        hook_arms.reverse();
+
+        quote_spanned! {span=>
+            #[allow(unused)]
+            StackEntry::Node(node @ (#pat)) => {
+                #(#leading_hook_stmts)*
+                #(#push_stmts)*
+            }
+            #(#hook_arms)*
+        }
+    }
 }
 
 pub fn impl_visitor_match(args: TokenStream) -> Result<TokenStream, Error> {
@@ -44,112 +147,16 @@ pub fn impl_visitor_match(args: TokenStream) -> Result<TokenStream, Error> {
             ));
         }
 
-        let mut chunks = Vec::new();
-        let mut pending_stmts = Vec::new();
+        let chunks = match *body {
+            Expr::Block(ExprBlock { block, .. }) => block_to_chunks(block)?,
 
-        match *body {
-            Expr::Block(ExprBlock {
-                block: Block { stmts, .. },
-                ..
-            }) => {
-                for stmt in stmts {
-                    match stmt {
-                        Stmt::Macro(StmtMacro {
-                            mac: Macro { path, tokens, .. },
-                            ..
-                        }) if paths::path_is_ident_str(&path, "visit") => {
-                            if !pending_stmts.is_empty() {
-                                chunks.push(Chunk::Hook(pending_stmts));
-                                pending_stmts = Vec::new();
-                            }
-
-                            let arg: Expr = syn::parse2(tokens)?;
-                            chunks.push(Chunk::Visit(arg));
-                        }
-
-                        other_stmt => {
-                            pending_stmts.push(other_stmt);
-                        }
-                    }
-                }
-
-                if !pending_stmts.is_empty() {
-                    chunks.push(Chunk::Hook(pending_stmts));
-                }
-            }
-
-            other_expr => {
-                chunks.push(Chunk::Hook(vec![Stmt::Expr(
-                    other_expr,
-                    Some(Token![;](span)),
-                )]));
-            }
-        }
-
-        let ent_arm = if chunks.is_empty() {
-            quote_spanned! {span=>
-                StackEntry::Node(#pat) => {}
-            }
-        } else {
-            // So we can treat it as a stack
-            chunks.reverse();
-
-            let first_chunk = chunks.pop().expect("chunks is nonempty yet empty");
-            let (leading_hook_stmts, extra_first_chunk) = if let Chunk::Hook(stmts) = first_chunk {
-                (stmts, None)
-            } else {
-                (Vec::new(), Some(first_chunk))
-            };
-
-            let mut hook_counter = 0usize;
-            let (push_stmts, mut hook_arms): (Vec<_>, Vec<_>) = extra_first_chunk
-                .into_iter()
-                .chain(chunks.into_iter())
-                .map(|chunk| {
-                    match chunk {
-                        Chunk::Visit(expr) => {
-                            (
-                                quote_spanned! {span=>
-                                    stack.push(StackEntry::Node(&#expr));
-                                },
-                                quote_spanned! {span=>
-                                    // No hook arm needed
-                                },
-                            )
-                        }
-
-                        Chunk::Hook(stmts) => {
-                            let hook_id = hook_counter;
-                            hook_counter += 1;
-
-                            (
-                                quote_spanned! {span=>
-                                    stack.push(StackEntry::Hook(#hook_id, node));
-                                },
-                                quote_spanned! {span=>
-                                    #[allow(unused)]
-                                    StackEntry::Hook(#hook_id, #pat) => {
-                                        #(#stmts)*
-                                    }
-                                },
-                            )
-                        }
-                    }
-                })
-                .unzip();
-            // For readability of generated code
-            hook_arms.reverse();
-
-            quote_spanned! {span=>
-                #[allow(unused)]
-                StackEntry::Node(node @ (#pat)) => {
-                    #(#leading_hook_stmts)*
-                    #(#push_stmts)*
-                }
-                #(#hook_arms)*
-            }
+            other_expr => vec![Chunk::Hook(vec![Stmt::Expr(
+                other_expr,
+                Some(Token![;](span)),
+            )])],
         };
-        ent_arms.push(ent_arm);
+
+        ent_arms.push(chunks_into_ent_arm(&pat, span, chunks));
     }
 
     Ok(quote_spanned! {span=>
