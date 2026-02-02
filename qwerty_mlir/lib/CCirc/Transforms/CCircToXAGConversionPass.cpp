14fd3705b006bb3a2bed089d2b29d0e0bed4a16f
Austin Adams <git@austinjadams.com>
2026-02-01 19:15:47 -0500

    Convert ccirc dialect to func/arith

--- qwerty_mlir/lib/CCirc/Transforms/CCircToXAGConversionPass.cpp
+++ qwerty_mlir/lib/CCirc/Transforms/CCircToXAGConversionPass.cpp
@@ -4,7 +4,6 @@
 #include "mlir/Transforms/DialectConversion.h"
 
 #include "CCirc/IR/CCircOps.h"
-#include "CCirc/Synth/CCircSynth.h"
 #include "CCirc/Transforms/CCircPasses.h"
 
 #include "PassDetail.h"
@@ -68,94 +67,6 @@ struct SplitMultiBitConstant
     }
 };
 
-void decomposeRotate(
-        mlir::ConversionPatternRewriter &rewriter,
-        mlir::Value value,
-        mlir::Value amt,
-        mlir::Operation *op,
-        ccirc::BitRotateDirection dir) {
-    mlir::Location loc = op->getLoc();
-    mlir::ValueRange value_wires = rewriter.create<ccirc::WireUnpackOp>(
-        loc, value).getWires();
-    llvm::SmallVector<mlir::Value> wires_n(value_wires);
-
-    mlir::ValueRange amount_wires = rewriter.create<ccirc::WireUnpackOp>(
-        loc, amt).getWires();
-    llvm::SmallVector<mlir::Value> wires_k(amount_wires);
-
-    llvm::SmallVector<mlir::Value> wires_out;
-    ccirc::synthBitRotate(rewriter, loc, dir, wires_n, wires_k, wires_out);
-    rewriter.replaceOpWithNewOp<ccirc::WirePackOp>(op, wires_out);
-}
-
-struct DecomposeRotateLeft
-        : public mlir::OpConversionPattern<ccirc::RotateLeftOp> {
-    using mlir::OpConversionPattern<ccirc::RotateLeftOp>::OpConversionPattern;
-
-    mlir::LogicalResult matchAndRewrite(
-            ccirc::RotateLeftOp rotl,
-            OpAdaptor adaptor,
-            mlir::ConversionPatternRewriter &rewriter) const final {
-        decomposeRotate(rewriter, adaptor.getValue(), adaptor.getAmount(),
-                        rotl, ccirc::BitRotateDirection::Left);
-        return mlir::success();
-    }
-};
-
-struct DecomposeRotateRight
-        : public mlir::OpConversionPattern<ccirc::RotateRightOp> {
-    using mlir::OpConversionPattern<ccirc::RotateRightOp>::OpConversionPattern;
-
-    mlir::LogicalResult matchAndRewrite(
-            ccirc::RotateRightOp rotr,
-            OpAdaptor adaptor,
-            mlir::ConversionPatternRewriter &rewriter) const final {
-        decomposeRotate(rewriter, adaptor.getValue(), adaptor.getAmount(),
-                        rotr, ccirc::BitRotateDirection::Right);
-        return mlir::success();
-    }
-};
-
-struct DecomposeModMul
-        : public mlir::OpConversionPattern<ccirc::ModMulOp> {
-    using mlir::OpConversionPattern<ccirc::ModMulOp>::OpConversionPattern;
-
-    mlir::LogicalResult matchAndRewrite(
-            ccirc::ModMulOp mod_mul,
-            OpAdaptor adaptor,
-            mlir::ConversionPatternRewriter &rewriter) const final {
-        mlir::Location loc = mod_mul.getLoc();
-        uint64_t x = mod_mul.getX();
-        uint64_t j = mod_mul.getJ();
-        uint64_t N = mod_mul.getModN();
-
-        // Go ahead and do the repeated squaring here, classically
-        // TODO: should use llvm::APInt here in case N is very large
-        uint64_t x_2j_modN = x % N;
-        for (uint64_t i = 1; i <= j; i++) {
-            x_2j_modN = (x_2j_modN * x_2j_modN) % N;
-        }
-
-        mlir::ValueRange y_wires = rewriter.create<ccirc::WireUnpackOp>(
-            loc, adaptor.getY()).getWires();
-        llvm::SmallVector<mlir::Value> wires_y(y_wires);
-        llvm::SmallVector<mlir::Value> wires_out;
-
-        // TODO: these should be APInts already as mentioned above. That is,
-        //       the AST node should also be updated to have dashu::IBigs, and
-        //       the ModMulOp itself should have APInt attributes instead of i64
-        //       attributes.
-        llvm::APInt x_bigint(BITS_NEEDED(x_2j_modN), x_2j_modN);
-        llvm::APInt modN_bigint(BITS_NEEDED(N), N);
-
-        ccirc::synthModMul(rewriter, loc, x_bigint, modN_bigint, wires_y,
-                           wires_out);
-
-        rewriter.replaceOpWithNewOp<ccirc::WirePackOp>(mod_mul, wires_out);
-        return mlir::success();
-    }
-};
-
 struct CCircToXAGConversionPass
         : public ccirc::CCircToXAGConversionBase<CCircToXAGConversionPass> {
     void runOnOperation() override {
@@ -185,10 +96,8 @@ struct CCircToXAGConversionPass
             });
 
         mlir::RewritePatternSet patterns(&getContext());
-        patterns.add<SplitMultiBitConstant,
-                     DecomposeRotateLeft,
-                     DecomposeRotateRight,
-                     DecomposeModMul>(&getContext());
+        ccirc::populateSynthConversionPatterns(patterns);
+        patterns.add<SplitMultiBitConstant>(&getContext());
 
         if (mlir::failed(mlir::applyFullConversion(circ, target, std::move(patterns)))) {
             signalPassFailure();
