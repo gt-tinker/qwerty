f7fb2552d96625c3eea9c1bef2c999508b168a99
Austin Adams <git@austinjadams.com>
2026-02-09 21:13:53 -0500

    Render simple qubit references

--- qwerty_ast/src/repl.rs
+++ qwerty_ast/src/repl.rs
@@ -3,14 +3,15 @@
 //! arXiv:2404.12603.
 
 use crate::ast::{
-    Canonicalizable, Stmt, angles_are_approx_equal,
+    Canonicalizable, Stmt, angle_is_approx_zero, angles_are_approx_equal, canon_angle,
     qpu::{
-        Adjoint, BitLiteral, Conditional, EmbedClassical, Expr, Predicated, QLit, QLitExpr,
-        QubitRef, Tensor, UnitLiteral, expr,
+        Adjoint, BitLiteral, Conditional, EmbedClassical, Expr, NonUniformSuperpos, Predicated,
+        QLit, QLitExpr, QubitRef, Tensor, UnitLiteral, expr,
     },
 };
-use dashu::integer::UBig;
-use num_complex::Complex64;
+use dashu::{base::BitTest, integer::UBig};
+use num_bigint::BigUint;
+use num_complex::{Complex64, ComplexFloat};
 use quantum_sparse_sim::QuantumSim;
 use qwerty_ast_macros::rebuild;
 use std::collections::HashMap;
@@ -35,8 +36,173 @@ impl SparseReplState {
             return None;
         }
 
-        // TODO: do stuff from notes
-        None
+        // Fast path: this is a one-qubit statevector already
+        if *num_qubits == 1 {
+            return match &statevec[..] {
+                [(bits, amp0)] if *bits == UBig::ZERO => Some((*amp0, Complex64::ZERO)),
+                [(bits, amp1)] if *bits == UBig::ONE => Some((Complex64::ZERO, *amp1)),
+                [(bits0, amp0), (bits1, amp1)] | [(bits1, amp1), (bits0, amp0)]
+                    if *bits0 == UBig::ZERO && *bits1 == UBig::ONE =>
+                {
+                    Some((*amp0, *amp1))
+                }
+                _ => unreachable!("Not a one-qubit unit vector"),
+            };
+        }
+
+        // Let ρ = |ψ⟩⟨ψ|, where |ψ⟩ is the entire n-qubit statevector, i.e.,
+        // |ψ⟩ = ∑_{i=0}^{2ⁿ−1} αᵢ.
+        //
+        // Then let ρₖ= tr₋ₖ(ρ). Here, tr₋ₖ(⋅) is notation for the partial
+        // trace as defined in Section 2.4.3 of Mike&Ike, with the ₋ₖ subscript
+        // meaning "trace out all qubits except for the qubit with index k".
+        //
+        // Our first step is to check if tr(ρₖ²) = 1, that is, if the qubit at
+        // index k is not entangled with any other qubits. Per some
+        // pen-and-paper perspiration,
+        // tr(ρₖ²) = ∑_{ℓ=0}^{2ⁿ⁻¹−1} ∑_{ℓ'=0}^{2ⁿ⁻¹−1}
+        //           ∑_{x=0}^1 ∑_{y=0}^1
+        //           α_{ℓxℓ} α_{ℓyℓ}* α_{ℓ'yℓ'} α_{ℓ'xℓ'}*
+        //         = ∑_{ℓ=0}^{2ⁿ⁻¹−1} ∑_{ℓ'=0}^{2ⁿ⁻¹−1}
+        //           α_{ℓ0ℓ} α_{ℓ0ℓ}* α_{ℓ'0ℓ'} α_{ℓ'0ℓ'}*
+        //           + α_{ℓ0ℓ} α_{ℓ1ℓ}* α_{ℓ'1ℓ'} α_{ℓ'0ℓ'}*
+        //           + α_{ℓ1ℓ} α_{ℓ0ℓ}* α_{ℓ'0ℓ'} α_{ℓ'1ℓ'}*
+        //           + α_{ℓ1ℓ} α_{ℓ1ℓ}* α_{ℓ'1ℓ'} α_{ℓ'1ℓ'}*
+        //         = ∑_{ℓ=0}^{2ⁿ⁻¹−1} ∑_{ℓ'=0}^{2ⁿ⁻¹−1}
+        //           |α_{ℓ0ℓ}|² |α_{ℓ'0ℓ'}|²
+        //           + α_{ℓ0ℓ} α_{ℓ1ℓ}* α_{ℓ'1ℓ'} α_{ℓ'0ℓ'}*
+        //           + (α_{ℓ0ℓ} α_{ℓ1ℓ}* α_{ℓ'1ℓ'} α_{ℓ'0ℓ'}*)*
+        //           + |α_{ℓ1ℓ}|² |α_{ℓ'1ℓ'}|²
+        //         = ∑_{ℓ=0}^{2ⁿ⁻¹−1} ∑_{ℓ'=0}^{2ⁿ⁻¹−1}
+        //           |α_{ℓ0ℓ}|² |α_{ℓ'0ℓ'}|²
+        //           + 2Re[α_{ℓ0ℓ} α_{ℓ1ℓ}* α_{ℓ'1ℓ'} α_{ℓ'0ℓ'}*]
+        //           + |α_{ℓ1ℓ}|² |α_{ℓ'1ℓ'}|²
+        //
+        // Beware, * here means complex conjugate, not multiplication. And the
+        // shitty ℓxℓ notation means bits [0,k) of ℓ, concatenated with x,
+        // concatenated with bits [k,n) of ℓ. Sorry.
+
+        // First, move the bit in question to the LSB (rightmost) position and
+        // sort the vector. Amplitudes whose bits differe only by the bit in
+        // question are adjacent in the sorted vector.
+        let bit_idx = *num_qubits - 1 - qubit_index;
+        let mut statevec: Vec<_> = statevec
+            .iter()
+            .map(|(bits, amp)| {
+                let split_at = bit_idx + 1;
+                let (mut lo, hi) = bits.clone().split_bits(split_at);
+                let bit = lo.bit(bit_idx);
+                lo.clear_bit(bit_idx);
+                lo <<= 1;
+                if bit {
+                    lo.set_bit(0);
+                }
+                let bits = hi << split_at | lo;
+                (bits, *amp)
+            })
+            .collect();
+        statevec.sort_by(|(lbits, _lamp), (rbits, _ramp)| lbits.cmp(rbits));
+
+        // Next, find pairs of adjacent amplitudes. `pairs` will contain pairs
+        // of nonzero amplitudes that differ only by the LSB. The first entry
+        // of the pair has a 0 LSB, and the second has a 1 LSB.
+        let mut pairs = Vec::new();
+        let mut i = 0;
+        loop {
+            if i + 1 >= statevec.len() {
+                break;
+            }
+            let (bits0, amp0) = &statevec[i];
+            let (bits1, amp1) = &statevec[i + 1];
+            if bits0 ^ bits1 == UBig::ONE {
+                pairs.push((*amp0, *amp1));
+                // Skip past r
+                i += 1;
+            }
+
+            i += 1;
+        }
+
+        // Calculate tr(ρₖ²) to find out if this qubit is entangled.
+        let mut trace = 0.0;
+
+        // Calculate the first and last terms in the sum above.
+        for (bits, amp) in statevec.iter() {
+            let bit = bits.bit(0);
+            for (bitsp, ampp) in statevec.iter() {
+                let bitp = bitsp.bit(0);
+                if bit == bitp {
+                    trace += amp.norm_sqr() * ampp.norm_sqr();
+                }
+            }
+        }
+
+        // Calculate the middle term in the sum above.
+        for (amp0, amp1) in pairs.iter() {
+            for (amp0p, amp1p) in pairs.iter() {
+                trace += 2.0 * (amp0 * amp1.conj() * amp1p * amp0p.conj()).re();
+            }
+        }
+
+        if !angles_are_approx_equal(trace, 1.0) {
+            // Entangled!
+            return None;
+        }
+        // Else, not entangled. There is hope.
+
+        // Next, we want to find a |ψₖ⟩ such that ρₖ = |ψₖ⟩⟨ψₖ|. By some
+        // pen-and-paper math, we have
+        // ρₖ= ∑_{x=0}^1 ∑_{y=0}^1 β_{xy}|x⟩⟨y|,
+        // where β_{xy} = ∑_{ℓ=0}^{2ⁿ⁻¹−1} α_{ℓxℓ} α_{ℓyℓ}*.
+        //
+        // To find |ψₖ⟩, we can do one of the two following computations:
+        //
+        //    ρₖ|0⟩      √(∑_{ℓ=0}^{2ⁿ⁻¹−1} |α_{ℓ0ℓ}|²) |0⟩ + ∑_{ℓ=0}^{2ⁿ⁻¹−1} α_{ℓ1ℓ} α_{ℓ0ℓ}* |1⟩
+        // ----------- =                                      ---------------------------------
+        //  √(⟨0|ρₖ|0⟩)                                         √(∑_{ℓ=0}^{2ⁿ⁻¹−1} |α_{ℓ0ℓ}|²)
+        //
+        // or
+        //
+        //    ρₖ|1⟩      ∑_{ℓ=0}^{2ⁿ⁻¹−1} α_{ℓ0ℓ} α_{ℓ1ℓ}* |0⟩ + √(∑_{ℓ=0}^{2ⁿ⁻¹−1} |α_{ℓ1ℓ}|²) |1⟩
+        // ----------- = ---------------------------------
+        //  √(⟨1|ρₖ|1⟩)    √(∑_{ℓ=0}^{2ⁿ⁻¹−1} |α_{ℓ1ℓ}|²)
+        //
+        // The right one to choose depends on which (if any) of the
+        // denominators √(⟨0|ρₖ|0⟩) and √(⟨1|ρₖ|1⟩) are 0.
+
+        let mut beta00 = 0.0;
+        let mut beta11 = 0.0;
+
+        for (bits, amp) in statevec {
+            if !bits.bit(0) {
+                // LSB is 0
+                beta00 += amp.norm_sqr();
+            } else {
+                // LSB is 1
+                beta11 += amp.norm_sqr();
+            }
+        }
+
+        // Decide which way to use to compute |ψₖ⟩.
+        if !angle_is_approx_zero(beta00) {
+            // Compute |ψₖ⟩ = ρₖ|0⟩/√(⟨0|ρₖ|0⟩).
+            let beta00hat = beta00.sqrt();
+            let mut beta10 = Complex64::ZERO;
+            for (amp0, amp1) in pairs {
+                beta10 += amp1 * amp0.conj();
+            }
+            let beta10hat = beta10 / beta00hat;
+            Some((beta00hat.into(), beta10hat))
+        } else {
+            // Compute |ψₖ⟩ = ρₖ|1⟩/√(⟨1|ρₖ|1⟩).
+            let beta11hat = beta11.sqrt();
+            let mut beta01 = Complex64::ZERO;
+            for (amp0, amp1) in pairs {
+                beta01 += amp0 * amp1.conj();
+            }
+            let beta01hat = beta01 / beta11hat;
+            Some((beta01hat, beta11hat.into()))
+        }
     }
 }
 
@@ -45,6 +211,22 @@ pub struct ReplState {
     sim: QuantumSim,
     // TODO: use this
     _bindings: HashMap<String, Expr>,
+}
+
+/// Swap both endianness and the bigint library used. Stefan (qir-runner)
+/// puts bit 0 on the rightmost bit (the LSB), but for us, bit 0 is the
+/// leftmost (MSB).
+fn stefan_bits_to_our_bits(stefan_bits: BigUint, num_bits: usize) -> UBig {
+    let mut our_bits = UBig::ZERO;
+    for bit_idx in 0..num_bits {
+        let bit = if stefan_bits.bit(bit_idx as u64) {
+            UBig::ONE
+        } else {
+            UBig::ZERO
+        };
+        our_bits = our_bits << 1 | bit;
+    }
+    our_bits
 }
 
 impl ReplState {
@@ -66,15 +248,14 @@ impl ReplState {
     }
 
     pub fn get_sparse_state(&mut self) -> SparseReplState {
-        // Convert from qir-runner bigint library (num_complex) to the one we
+        // Convert from qir-runner bigint library (num_bigint) to the one we
         // use (dashu, a fork that is supposedly faster)
         let (stefan_state, num_qubits) = self.sim.get_state();
         let statevec = stefan_state
             .into_iter()
-            .map(|(bits, amplitude)| {
-                let bytes_le = bits.to_bytes_le();
-                let dashu_bits = UBig::from_le_bytes(&bytes_le);
-                (dashu_bits, amplitude)
+            .map(|(stefan_bits, amplitude)| {
+                let our_bits = stefan_bits_to_our_bits(stefan_bits, num_qubits);
+                (our_bits, amplitude)
             })
             .collect();
         SparseReplState {
@@ -93,11 +274,67 @@ impl Expr {
     pub(crate) fn recover_rewriter(self, state: &SparseReplState) -> Self {
         match self {
             Expr::QubitRef(QubitRef { index }) => {
-                // TODO: actually do a partial trace etc
-                Expr::QLitExpr(QLitExpr {
-                    qlit: QLit::ZeroQubit { dbg: None },
-                    dbg: None,
-                })
+                if let Some((amp0, amp1)) = state.try_extract_1q_state(index) {
+                    let (zero_abs, zero_rad) = amp0.to_polar();
+                    let zero_prob = zero_abs * zero_abs;
+                    let zero_deg = canon_angle(zero_rad * std::f64::consts::FRAC_1_PI * 180.0);
+
+                    let (one_abs, one_rad) = amp1.to_polar();
+                    let one_prob = one_abs * one_abs;
+                    let one_deg = canon_angle(one_rad * std::f64::consts::FRAC_1_PI * 180.0);
+
+                    let zero = QLit::ZeroQubit { dbg: None };
+                    let zero_vec = if angle_is_approx_zero(zero_deg) {
+                        zero
+                    } else {
+                        QLit::QubitTilt {
+                            q: Box::new(zero),
+                            angle_deg: zero_deg,
+                            dbg: None,
+                        }
+                    };
+
+                    let one = QLit::OneQubit { dbg: None };
+                    let one_vec = if angle_is_approx_zero(one_deg) {
+                        one
+                    } else {
+                        QLit::QubitTilt {
+                            q: Box::new(one),
+                            angle_deg: one_deg,
+                            dbg: None,
+                        }
+                    };
+
+                    if angle_is_approx_zero(zero_prob) {
+                        Expr::QLitExpr(QLitExpr {
+                            qlit: one_vec,
+                            dbg: None,
+                        })
+                    } else if angle_is_approx_zero(one_prob) {
+                        Expr::QLitExpr(QLitExpr {
+                            qlit: zero_vec,
+                            dbg: None,
+                        })
+                    } else if angles_are_approx_equal(zero_prob, 0.5) {
+                        let uniform = QLit::UniformSuperpos {
+                            q1: Box::new(zero_vec),
+                            q2: Box::new(one_vec),
+                            dbg: None,
+                        };
+                        Expr::QLitExpr(QLitExpr {
+                            qlit: uniform,
+                            dbg: None,
+                        })
+                    } else {
+                        Expr::NonUniformSuperpos(NonUniformSuperpos {
+                            pairs: vec![(zero_prob, zero_vec), (one_prob, one_vec)],
+                            dbg: None,
+                        })
+                    }
+                } else {
+                    // Pass through unchanged
+                    Expr::QubitRef(QubitRef { index })
+                }
             }
             other_expr => other_expr,
         }
