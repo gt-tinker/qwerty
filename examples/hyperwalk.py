f0a319359deb56cfef1463d79246a85bb2fe1e86
Austin Adams <git@austinjadams.com>
2026-03-04 09:30:35 -0500

    Add walk on hypercube in 4D

---
+++ examples/hyperwalk.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import sys
+import random
+from qwerty import *
+
+def quantum_walk(steps, shots):
+    @qpu
+    def walk_step(q):
+        coin = std**2 * '????' >> fourier[2] * '????'
+
+        return (q | coin
+                  | (flip if '00_???' else id)
+                  | (flip if '01?_??' else id)
+                  | (flip if '10??_?' else id)
+                  | (flip if '11???_' else id))
+
+    @qpu
+    def kernel():
+        return ('00'*'0000' | (walk_step for i in range(steps))
+                            | discard**2 * measure**4)
+
+    print('Quantum walk:')
+    histogram(kernel(shots=shots))
+
+def classical_walk(steps, shots):
+    histo = {}
+    for _ in range(shots):
+        state = 0b0000
+        for i in range(steps):
+            coin_flip = random.randint(0, 3)
+            state ^= 1 << (3 - coin_flip)
+        state = bit[4](state)
+        histo[state] = histo.get(state, 0) + 1
+
+    print('Classical walk:')
+    histogram(histo)
+
+def main(args):
+    steps = int(args[0]) if args else 32
+    shots = int(args[1]) if len(args) > 1 else 1024
+
+    quantum_walk(steps, shots)
+    print()
+    classical_walk(steps, shots)
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
