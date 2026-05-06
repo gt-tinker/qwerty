2d9344a3d9a817aa2bb8ff826c65770323c139dd
Austin Adams <git@austinjadams.com>
2026-05-05 21:59:29 -0400

    Remove fake synthesis code for testing adder

--- qwerty_mlir/lib/CCirc/Synth/SynthArith.cpp
+++ qwerty_mlir/lib/CCirc/Synth/SynthArith.cpp
@@ -62,7 +62,6 @@ void synthAdd(
     fullAdderN(builder, loc, wires_a, wires_b, zero, wires_sum);
 }
 
-// make it take wires_y, does mod doubling on wires y and put on wires out.
 void synthModMul(
         mlir::OpBuilder &builder,
         mlir::Location loc,
@@ -70,32 +69,8 @@ void synthModMul(
         llvm::APInt modN,
         llvm::SmallVectorImpl<mlir::Value> &wires_y,
         llvm::SmallVectorImpl<mlir::Value> &wires_out) {
-
-    // for testing fullAdderN
-    // wires_y = cin, a, b
-    llvm::SmallVector<mlir::Value> aBits;
-    llvm::SmallVector<mlir::Value> bBits;
-    mlir::Value cin = wires_y[15];
-    mlir::Value zero = builder.create<ccirc::ConstantOp>(
-        loc, llvm::APInt(/*numBits=*/1, /*val=*/0)).getResult();
-
-    // set aBits and bBits
-    for (size_t i = 0; i < 8; i++){
-        aBits.push_back(wires_y[i+16]);
-        bBits.push_back(wires_y[i+24]);
-    }
-
-    for (size_t i = 0; i < wires_y.size() - 9; i++){
-        wires_out.push_back(zero);
-    }
-
-    // call full adder
-    llvm::SmallVector<mlir::Value> sumBits;
-    auto carryOut = fullAdderN(builder, loc, aBits, bBits, cin, sumBits);
-
-    // clear output and push sum then carryout
-    wires_out.push_back(carryOut);
-    wires_out.append(sumBits.begin(), sumBits.end());
+    // TODO: implement me
+    assert(0 && "Mod mul not implemented");
 }
 
 } // namespace ccirc
